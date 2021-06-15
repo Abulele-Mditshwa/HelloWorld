9cdbf0f80d6c226f6f2e301bc94995dad751196d
Sandise Mditshwa <sandisemditshwa@smditshw-m-94cs.home>
2021-06-15 11:22:32 +0200

    Push V1 HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+# Python 
+
+
+print('Please tell me your name !')
+name = input()
+
+
+# In[7]:
+
+
+print(" Hello "+ name) 
+
+
+# In[ ]:
+
+
+
+
